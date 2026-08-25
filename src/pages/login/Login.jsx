9a119bda306f89c4cd397be5757b87a8313918f1
João Zacchello <xongs@Joaos-MacBook-Air.local>
2026-08-25 15:30:44 -0300

    .

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -7,43 +7,74 @@ export default function Login() {
     const [username, setUsername] = useState("")
     const [password, setPassword] = useState("")
 
+    const [alrHaveAcc, setAlrHaveAcc] = useState(false)
+
     return (
         <div className="content">
-            <form>
-                <label>
-                    Primeiro nome: 
-                    <input
-                        type="text"
-                        placeholder="Seu nome aqui..."
-                        onChange={(e) => setFirstname(e.target.value)}
-                    />
-                </label>
-                <label>
-                    Sobrenome:
-                    <input
-                        type="text"
-                        placeholder="Seu sobrenome aqui..."
-                        onChange={(e) => setLastName(e.target.value)}
-                    />
-                </label>
-                <label>
-                    Nome de usuário:
-                    <input
-                        type="text"
-                        placeholder="Digite seu username..."
-                        onChange={(e) => setUsername(e.target.value)}
-                    />
-                </label>
-                <label>
-                    Senha *máx 8 caracteres e somente letras e numeros:
-                    <input
-                        type="text"
-                        placeholder="Digite sua senha..."
-                        maxLength={8}
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                </label>
-            </form>
+            {alrHaveAcc ? (
+                <form>
+                    <label>
+                        Primeiro nome: 
+                        <input
+                            type="text"
+                            placeholder="Seu nome aqui..."
+                            onChange={(e) => setFirstname(e.target.value)}
+                        />
+                    </label>
+                    <label>
+                        Sobrenome:
+                        <input
+                            type="text"
+                            placeholder="Seu sobrenome aqui..."
+                            onChange={(e) => setLastName(e.target.value)}
+                        />
+                    </label>
+                    <label>
+                        Nome de usuário:
+                        <input
+                            type="text"
+                            maxLength={16}
+                            placeholder="Digite seu username..."
+                            onChange={(e) => setUsername(e.target.value)}
+                        />
+                    </label>
+                    <label>
+                        Senha *máx 8 caracteres e somente letras e numeros:
+                        <input
+                            type="text"
+                            placeholder="Digite sua senha..."
+                            maxLength={8}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                    </label>
+
+
+                <p>Já tem uma conta? <span className="link-span" onClick={() => setAlrHaveAcc(false)}>Login</span></p>
+                </form>
+            ) : (
+                <form>
+                    <label>
+                        Nome de usuário:
+                        <input
+                            type="text"
+                            placeholder="Digite seu nome de usuário..."
+                            maxLength={16}
+                            onChange={(e) => setUsername(e.target.value)}
+                        />
+                    </label>
+                    <label>
+                        Senha:
+                        <input
+                            type="text"
+                            placeholder="Digite sua senha..."
+                            maxLength={8}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                    </label>
+                    
+                    <p>Ainda não tem conta? <span className="link-span" onClick={() => setAlrHaveAcc(true)}>Registrar</span></p>
+                </form>
+            )}
         </div>
     )
 }
