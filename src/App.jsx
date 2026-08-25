d2ee98a9e23caa21bc59b66be81c974b57bb0e94
João Zacchello <xongs@Joaos-MacBook-Air.local>
2026-08-25 15:31:13 -0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { HashRouter, Routes, Route } from "react-router-dom"
 import Clicker from "./pages/clicker/Clicker"
+import Login from "./pages/login/Login"
 
 export default function App() {
     return (
         <HashRouter>
             <Routes>
                 <Route path="/" element={<Clicker />} />
+                <Route path="/login" element={<Login />} />
             </Routes>
         </HashRouter>
     )
